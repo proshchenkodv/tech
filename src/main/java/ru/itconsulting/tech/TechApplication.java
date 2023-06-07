78c6e8ba52454491b4b21afedd9fb328dbcb3790
klarkken@yandex.ru <OcGojCekamefby3>
2023-06-07 19:12:30 +0300

    second commit

--- src/main/java/ru/itconsulting/tech/TechApplication.java
+++ src/main/java/ru/itconsulting/tech/TechApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"ru.itconsulting.tech"}, exclude = {
         DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class
 })
+@EnableJpaRepositories(basePackages = "ru.itconsulting.tech")
 public class TechApplication {
 
     public static void main(String[] args) {
--- src/main/java/ru/itconsulting/tech/dao/repository/StationPathRepository.java
+++ src/main/java/ru/itconsulting/tech/dao/repository/StationPathRepository.java
@@ -2,8 +2,9 @@ package ru.itconsulting.tech.dao.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.itconsulting.tech.dao.entity.StationPath;
 
 
 @Repository
-public interface StationPath extends JpaRepository<StationPath, Long> {
+public interface StationPathRepository extends JpaRepository<StationPath, Long> {
 }

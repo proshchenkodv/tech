ca24fb608a485c9a123bafb71d12d9684fd903b8
klarkken@yandex.ru <OcGojCekamefby3>
2023-06-26 14:40:29 +0300

    second commit

--- src/main/resources/db.changelog/Triggers/3_dct_bloc_biu.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
---changeset proshchenkodv:SSD-19 runOnChange:true splitStatements:false
-DROP TRIGGER IF EXISTS dct_bloc_biu ON rzd_wagon.dct_bloc;
-CREATE TRIGGER dct_bloc_biu
-    BEFORE INSERT OR UPDATE
-    ON rzd_wagon.dct_bloc
-    FOR EACH ROW
-EXECUTE FUNCTION rzd_wagon.for_trigger_biu();
